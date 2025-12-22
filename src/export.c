b0eae9ee5642927e647602c3974c9a20f15ea9dd
Manh-hello <manhnd.23it@vku.udn.vn>
2025-12-23 01:54:39 +0700

    Update export.c

--- src/export.c
+++ src/export.c
@@ -30,7 +30,6 @@ void get_current_time_str(char *buffer) {
     printf("UTC   : %s", asctime(gmtime(&now)));
     printf("Local : %s", asctime(localtime(&now)));
     
-    time_t now = time(NULL); 
     struct tm *t = localtime(&now);
     sprintf(buffer, "%02d:%02d:%02d", t->tm_hour, t->tm_min, t->tm_sec);
 }
